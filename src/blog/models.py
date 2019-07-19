9b4099ce757e2e8b51aa340a8c521b3c567014c9
aparna <aparna.bs12@gmail.com>
2019-07-19 09:40:46 +0530

    changed f-string to format()

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,9 +1,8 @@
-from django.db import models
 from django.conf import settings
-from django.utils import timezone
+from django.db import models
 from django.db.models import Q
+from django.utils import timezone
 # Create your models here.
-
 
 User = settings.AUTH_USER_MODEL
 
@@ -14,15 +13,16 @@ class BlogPostQuerySet(models.QuerySet):
         return self.filter(publish_date__lte=now)
 
     def search(self, query):
-    	lookup = (
-    		Q(title__icontains=query) |
+        lookup = (
+                    Q(title__icontains=query) |
                     Q(content__icontains=query) |
                     Q(slug__icontains=query) |
                     Q(user__first_name__icontains=query) |
                     Q(user__last_name__icontains=query) |
                     Q(user__username__icontains=query)
                     )
-    	return self.filter(lookup)
+
+        return self.filter(lookup)
 
 
 class BlogPostManager(models.Manager):
@@ -33,32 +33,32 @@ class BlogPostManager(models.Manager):
         return self.get_queryset().published()
 
     def search(self, query=None):
-    	if query is None:
-    		return sefl.get_queryset.none()
-    	return self.get_queryset().published().search(query)
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().published().search(query)
 
 
-class BlogPost(models.Model):
-	# id = models.foIntergerField() #pk
-	user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
-	image = models.ImageField(upload_to='image/',blank=True,null=True)
-	title = models.CharField(max_length=120)
-	slug = models.SlugField(unique=True)
-	content = models.TextField(null=True, blank=True)
-	publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
-	timestamp = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
+class BlogPost(models.Model): # blogpost_set -> queryset
+    # id = models.IntegerField() # pk
+    user    = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
+    image   = models.ImageField(upload_to='image/', blank=True, null=True)
+    title  = models.CharField(max_length=120)
+    slug   = models.SlugField(unique=True) # hello world -> hello-world
+    content  = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
-	objects = BlogPostManager()
+    objects = BlogPostManager()
 
-	class Meta:
-	    ordering = ['-publish_date', '-updated', '-timestamp']
+    class Meta:
+        ordering = ['-publish_date', '-updated', '-timestamp']
 
-	def get_absolute_url(self):
-	    return f"/blog/{self.slug}"
+    def get_absolute_url(self):
+        return "/blog/{slug}.format(self.slug)"
+    
+    def get_edit_url(self):
+        return {url}/edit.format(self.get_absolute_url())
 
-	def get_edit_url(self):
-	    return f"{self.get_absolute_url()}/edit"
-
-	def get_delete_url(self):
-	    return f"{self.get_absolute_url()}/delete"	
+    def get_delete_url(self):
+        return {url}/delete.format(self.get_absolute_url())
